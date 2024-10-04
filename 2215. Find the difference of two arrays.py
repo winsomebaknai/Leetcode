7b59dd5d4f9abb076af219a38585aa259ecca4d9
winsome_baknai <54034787+winsomebaknai@users.noreply.github.com>
2024-10-04 12:27:44 +0530

    leetcode 75 (easy / HashSet)

---
+++ 2215. Find the difference of two arrays.py
@@ -0,0 +1,40 @@
+'''
+2215. Find the difference of two array
+Given two 0-indexed integer arrays nums1 and nums2, return a list answer of size 2 where:
+
+answer[0] is a list of all distinct integers in nums1 which are not present in nums2.
+answer[1] is a list of all distinct integers in nums2 which are not present in nums1.
+Note that the integers in the lists may be returned in any order.
+
+ 
+
+Example 1:
+
+Input: nums1 = [1,2,3], nums2 = [2,4,6]
+Output: [[1,3],[4,6]]
+Explanation:
+For nums1, nums1[1] = 2 is present at index 0 of nums2, whereas nums1[0] = 1 and nums1[2] = 3 are not present in nums2. Therefore, answer[0] = [1,3].
+For nums2, nums2[0] = 2 is present at index 1 of nums1, whereas nums2[1] = 4 and nums2[2] = 6 are not present in nums2. Therefore, answer[1] = [4,6].
+
+'''
+class Solution:
+    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        set1 = set(nums1)
+        set2 = set(nums2)
+
+        result1 = set()
+        result2 = set()
+
+        for n in nums1:
+            if n not in set2:
+                result1.add(n)
+        
+        for n in nums2:
+            if n not in nums1:
+                result2.add(n)
+
+
+
+        return [list(result1), list(result2)]
+
+        
