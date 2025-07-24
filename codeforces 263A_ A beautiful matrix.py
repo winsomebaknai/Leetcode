731479e5912bf7ceb223399bfeb6fa3920b0e6f1
winsome_baknai <54034787+winsomebaknai@users.noreply.github.com>
2025-07-24 23:40:00 +0530

    codeforces 263/A (Matrix Manipulation)

---
+++ codeforces 263A_ A beautiful matrix.py
@@ -0,0 +1,9 @@
+matrix = []
+for i in range(5):
+    row = list(map(int, input().split()))
+    matrix.append(row)
+
+for i in range(5):
+    for j in range(5):
+        if matrix[i][j] == 1:
+            print(abs(i - 2) + abs(j - 2))
